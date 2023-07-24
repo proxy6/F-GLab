be8cf46b34019c24ad5ee17218cfeeb18dd4bb83
proxy6 <progresseze@gmail.com>
2023-07-24 04:00:47 +0100

    made changes to team section

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs")
 const path = require('path')
 const app = express();
-
+const {sendMessage}  = require("./sendMessage");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -15,9 +15,7 @@ app.get('/', (req, res)=> {
     res.render('index')
 })
 
-// app.get('/contact', (req, res) => {
-//     res.render('contact');
-// })
+
 
 app.post('/contact-us', async (req, res) => {
     let resp =  await sendMessage(req.body);
